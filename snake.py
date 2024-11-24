5a8cf721df7385b56fb27a3f2b4311bfbfc24b36
seeratkawr <sman833@aucklanduni.ac.nz>
2024-11-25 11:06:11 +1300

    window shows up when run

--- snake.py
+++ snake.py
@@ -8,3 +8,9 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
 
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+window.mainloop()
