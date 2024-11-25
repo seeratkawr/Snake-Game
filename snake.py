19d0e05cd96e0ac8eea1877eeb86306e2bbb5d2c
seeratkawr <sman833@aucklanduni.ac.nz>
2024-11-26 11:47:24 +1300

    code the initial snake

--- snake.py
+++ snake.py
@@ -7,13 +7,24 @@ GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "green"
+SNAKE_COLOR = "blue"
 FOOD_COLOR = "red"
-BACKGROUND_COLOR = "black"
+BACKGROUND_COLOR = "green"
 
 #classes
 class Snake:
-  pass
+  def __init__(self):
+    self.body_size = BODY_PARTS
+    self.coordinates = []
+    self.squares = []
+
+    for i in range(0, BODY_PARTS):
+      self.coordinates.append([0, 0])
+    
+    for x, y in self.coordinates:
+      square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR, tag="snake")
+      self.squares.append(square)
+
 
 class Food:
   def __init__(self):
