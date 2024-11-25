5e0a07548a849f2768134631464037be67563ecb
seeratkawr <sman833@aucklanduni.ac.nz>
2024-11-26 11:07:46 +1300

    added constants

--- snake.py
+++ snake.py
@@ -1,16 +1,28 @@
 from tkinter import * #gui lib
 import random #to place food
 
+#constants
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "green"
+FOOD_COLOR = "red"
+BACKGROUND_COLOR = "black"
+
+#classes
 class Snake:
   pass
 
 class Food:
   pass
 
+#functions
 def next_turn():
   pass
 
-def change_direction():
+def change_direction(new_direction):
   pass
 
 def check_collisions():
